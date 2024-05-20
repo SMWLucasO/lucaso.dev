35368aa4932de599f8b186ccae6794a4073fc5af
Lucas Ouwens <lucas.ouwens@cocreate.nl>
2024-05-20 09:31:59 +0200

    Working on Tanstack router configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// Create a new router instance
+const router = createRouter({ routeTree });
 
 const root = document.getElementById("root");
 
 if (root) {
 	ReactDOM.createRoot(root).render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
 	);
 } else {
 	throw new Error("Could not find React root component");
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import million from "million/compiler";
+import path from "path";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
+import million from "million/compiler";
 import { defineConfig } from "vite";
+
+const __dirname = path.dirname(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [million.vite({ auto: true }), react()],
+	base: "/",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@shared": path.resolve(__dirname, "./src/Modules/Shared"),
+			"@components": path.resolve(__dirname, "./src/Modules/Shared/Components"),
+		},
+	},
+	plugins: [million.vite({ auto: true }), react(), TanStackRouterVite()],
+	server: {
+		port: 3000,
+	},
 });
